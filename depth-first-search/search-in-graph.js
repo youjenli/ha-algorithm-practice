07bf1a4a74bd1db7900ddfa6809c308b87e2ca14
youjenli <youjenli1124@gmail.com>
2020-07-29 15:51:17 +0800

    初步修訂深度優先找點搜尋

--- depth-first-search/search-in-graph.js
+++ depth-first-search/search-in-graph.js
@@ -8,27 +8,29 @@ const linksBetweenPoints = [
 //搜尋目標 4 號球
 const points = [1, 1, 1, 1, 1];
 let sum = 0;
+const path = [];
 
 const dfs = (point, target) => {
-    points[point] = 0;
     if (point == target) {//找到目標球
-        console.log(`找到目標球 ${target + 1}`);
-        return true;
+        console.log(`找到目標點 ${target + 1}`);
+        console.log(`總步數: ${sum}, 路徑 ${path}`);
+        return;
     }
     for (let i = 0 ; i < linksBetweenPoints[0].length ; i ++) {
         if ( linksBetweenPoints[point][i] != 0 && //排除自己
             linksBetweenPoints[point][i] == 1 && //有路能走
             points[i] == 1 ) {//還沒走過
-            console.log(`查找點 ${point+1}`);
+            points[i] = 0;
+            path.push(i + 1);
             sum++;
-            if (dfs(i, target)) {
-                return true;
-            };
+            dfs(i, target);
+            path.pop();
+            sum --;
+            points[i] = 1;
         }
     }
-    return false;
 }
 
-if(dfs(0, 3)) {
-    console.log(`總計步數: ${sum}`);
-}
+points[0] = 0;
+path.push(1);
+dfs(0, 3);
