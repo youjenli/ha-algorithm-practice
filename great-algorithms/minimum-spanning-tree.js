d743cb131c6b35da93aa7c61f3f3424e20efcffe
youjenli <youjenli1124@gmail.com>
2020-08-19 16:54:03 +0800

    加入最小生成樹的練習

--- great-algorithms/minimum-spanning-tree.js
+++ great-algorithms/minimum-spanning-tree.js
@@ -1,3 +1,5 @@
+/* 註： 執行此程式時，要加上參數 --harmony 和 --use-strict，否則後面遞迴部分可能導致程式掛掉。
+*/
 const readline = require('readline');
 const process = require('process');
 const rl = readline.createInterface({
@@ -8,6 +10,7 @@ const rl = readline.createInterface({
 let initailized = false;
 let items = null;
 let routes = null;
+let maxRoutes = null;
 let pointer = 0;
 
 rl.on('line', (input) => {
@@ -22,7 +25,8 @@ rl.on('line', (input) => {
                 for (let i = 0 ; i < items.length ; i ++) {
                     items[i] = i;
                 }
-                routes = new Array(itemCount);
+                routes = [];
+                maxRoutes = itemCount * (itemCount - 1) / 2;
                 initailized = true;
             }
         } else if (tokens[0] == 'ok')  {
@@ -36,49 +40,72 @@ rl.on('line', (input) => {
                         return -1;
                     }
                 });
+                console.log(`sorted order:`);
+                routes.forEach((route, idx) => {
+                    console.log(`${idx} ： ${route[0] + 1}, ${route[1] + 1}, ${route[2]}`);
+                });
 
-                const setParent = (idx) => {
-                    let promise = null;
-                    if (items[idx] != idx) {
-                        promise = new Promise(resolve => {
-                            setTimeout(() => {
-                                setParent(items[idx])
-                                    .then(() => {
-                                        resolve();
-                                    });
-                            }, 0);
-                        })
+                const setParent = (parent, childIdx) => {
+                    if (items[childIdx] != childIdx) {
+                        //要被連接的節點有其父節點，因此要連帶令其父節點切換父節點至新的父節點
+                        setParent(parent, items[childIdx]);
                     }
-                    if (promise != null) {
-                        promise.then(() => {
-                            console.log(`將 items[${idx}] 從 ${items[idx]} 設為 ${ancestor}`);
-                            items[idx] = ancestor;
-                            console.log(`${items}`);
-                        });
+                    items[childIdx] = parent;
+                }
+
+                let lengthOfPath = 0;
+                const leastRoute = items.length - 1;
+                let count = 0; //已採用的線路
+                for (let index = 0 /* 目前讀取的路徑排序結果 */ ; index < routes.length && count < leastRoute ; index ++) {
+                    console.log(`${index} : ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                    console.log(`items[routes[${index}][0]] ${items[routes[index][0]]} vs items[routes[${index}][1]] ${items[routes[index][1]]}`);
+                    if (items[routes[index][0]] == routes[index][0]) {
+                        if (items[routes[index][1]] == routes[index][1]) {
+                            /* 路徑首尾的父節點都是自己，直接把尾端列入新線路 */
+                            console.log(`採用 ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                            setParent(items[routes[index][0]], routes[index][1]);
+                            count++;
+                            lengthOfPath += routes[index][2];
+                        } else {
+                            /* 路徑首的父節點是自己，尾則不是 */
+                            if (items[routes[index][0]] != items[routes[index][1]]) {
+                                /* 但首尾的父節點不同，因此把首節點併至尾節點形成的路線中 */
+                                console.log(`採用 ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                                setParent(items[routes[index][1]], routes[index][0]);
+                                count++;
+                                lengthOfPath += routes[index][2];
+                            } else {
+                                /* 但尾的父節點是首，這表示兩者已在同一線路中 */
+                                console.log(`不採用 ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                            }
+                        }
                     } else {
-                        return new Promise(resolve => {
-                            console.log(`將 items[${idx}] 從 ${items[idx]} 設為 ${ancestor}`);
-                            items[idx] = ancestor;
-                            console.log(`${items}`);
-                            resolve();
-                        });
+                        // 首節點已被加到其他路線中
+                        if (items[routes[index][1]] == routes[index][1]) {
+                            /* 而尾節點不在任何路線中，因此把它加到首節點所在的線路 */
+                           console.log(`採用 ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                           setParent(items[routes[index][0]], routes[index][1]);
+                           count++;
+                           lengthOfPath += routes[index][2];
+                        } else if (items[routes[index][0]] != items[routes[index][1]]) {
+                            /* 尾節點在暨有路線中，而且該路線與首節點所屬的路線不同，
+                               這表示需要透過重設首或尾節點的父節點，以及目前父節點的父節點至另一條線路的根以便把兩線路串起來。
+                            */
+                            console.log(`採用 ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                            setParent(items[routes[index][0]], routes[index][1]);
+                            count++;
+                            lengthOfPath += routes[index][2];
+                        } else {
+                            /* 尾節點在暨有路線中，而且該路線與首節點所屬的路線相同，
+                               這表示兩點已在暨有的線路中，故不採用此線路。
+                            */
+                            console.log(`不採用 ${routes[index][0] + 1}, ${routes[index][1] + 1}, ${routes[index][2]}`);
+                        }
                     }
-                    return promise;
                 }
-                // setParent(descendent)
-                for ( let i = 0 ; i < items.length - 1 ; i ++) {
-                    setParent(items[routes[i][1]]);
-                }
-
-                // let count = 0;
-                // for ( let i = 0 ; i < items.length ; i ++) {
-                //     if (items[i] == i) {
-                //         count ++;
-                //     }
-                // }
-                // console.log(`共有 ${count} 個集團`);
-                // console.log(`${items}`);
-                // process.exit();
+                console.log(`length of path : ${lengthOfPath}`);
+                console.log(`${items}`);
+                process.exit();
             } else {
                 console.error('app has not been initialized.');
                 process.exit();
@@ -88,11 +115,11 @@ rl.on('line', (input) => {
             process.exit();
         }
     } else if (tokens.length == 3 && initailized == true) {
-        let startPoint = parseInt(tokens[0]), destination = parseInt(tokens[1])
-                , lengthOfRoute = parseInt(tokens[2]);
+        let startPoint = parseInt(tokens[0]), destination = parseInt(tokens[1]),
+            lengthOfRoute = parseInt(tokens[2]);
         if (!isNaN(startPoint) && !isNaN(destination) && !isNaN(lengthOfRoute)
                 && startPoint <= items.length && destination <= items.length
-                && pointer < routes.length) {
+                && pointer < maxRoutes) {
                 startPoint --, destination --;
                 routes[pointer] = [startPoint, destination, lengthOfRoute];
                 pointer ++;
